25972285c44236285abbdca0b69709eab7b5b34c
Artem Kravchenko <lan143@lan143.ru>
2026-06-13 17:47:28 +0300

    Some sensors in living room support

--- src/living_room/living_room.cpp
+++ src/living_room/living_room.cpp
@@ -1,3 +1,10 @@
+#include <binary_sensor/mtd262mb.h>
+#include <sensor/wbmsw_air_quality.h>
+#include <sensor/wbmsw_co2.h>
+#include <sensor/wbmsw_humidity.h>
+#include <sensor/wbmsw_light_level.h>
+#include <sensor/wbmsw_temperature.h>
+
 #include "./living_room.h"
 
 using namespace LivingRoom;
@@ -11,6 +18,69 @@ void LivingRoom::LivingRoom::init(Config config, EDHA::Device* device, EDWB::MR6
     if (!mr6c->setInputMode(MR6C_CHANNEL_SIX, EDWB::MR6C_INPUT_MODE_FREQUENCY)) {
         LOGE("init", "failed to set input mode for channel MR6C_CHANNEL_SIX");
     }
+
+    auto msw = _modbus->addMSW(config.modbusAddressWBMSW);
+    auto mtd262mb = _modbus->addMTD262MB(config.modbusAddressMTD262MB);
+
+    _temperature = new EDCommon::Sensor::WBMSWTemperature(msw);
+    _temperature->init(2, 10000, {
+        EDCommon::Sensor::withMQTT(
+            _mqtt,
+            config.mqttTopicPrefix,
+            "alfred",
+            "Living room temperature"
+        ), EDCommon::Sensor::withDiscovery(_discoveryMgr, device)
+    });
+
+    _humidity = new EDCommon::Sensor::WBMSWHumidity(msw);
+    _humidity->init(2, 10000, {
+        EDCommon::Sensor::withMQTT(
+            _mqtt,
+            config.mqttTopicPrefix,
+            "alfred",
+            "Living room humidity"
+        ), EDCommon::Sensor::withDiscovery(_discoveryMgr, device)
+    });
+
+    _airQuality = new EDCommon::Sensor::WBMSWAirQuality(msw);
+    _airQuality->init(0, 10000, {
+        EDCommon::Sensor::withMQTT(
+            _mqtt,
+            config.mqttTopicPrefix,
+            "alfred",
+            "Living room air quality"
+        ), EDCommon::Sensor::withDiscovery(_discoveryMgr, device)
+    });
+
+    _co2 = new EDCommon::Sensor::WBMSWCO2(msw);
+    _co2->init(0, 10000, {
+        EDCommon::Sensor::withMQTT(
+            _mqtt,
+            config.mqttTopicPrefix,
+            "alfred",
+            "Living room CO2"
+        ), EDCommon::Sensor::withDiscovery(_discoveryMgr, device)
+    });
+
+    _lightLevel = new EDCommon::Sensor::WBMSWLightLevel(msw);
+    _lightLevel->init(0, 10000, {
+        EDCommon::Sensor::withMQTT(
+            _mqtt,
+            config.mqttTopicPrefix,
+            "alfred",
+            "Living room light level"
+        ), EDCommon::Sensor::withDiscovery(_discoveryMgr, device)
+    });
+
+    _humanDetector = new EDCommon::BinarySensor::MTD262MB(mtd262mb);
+    _humanDetector->init(500, {
+        EDCommon::BinarySensor::withMQTT(
+            _mqtt,
+            config.mqttTopicPrefix,
+            "alfred",
+            "Living room human detected"
+        ), EDCommon::BinarySensor::withDiscovery(_discoveryMgr, device)
+    });
 
     auto livingRoomRelay = new EDCommon::Relay::WBMR6C(mr6c);
     livingRoomRelay->init(MR6C_RELAY_CHANNEL_LIVING_ROOM_LIGHT, {});
@@ -43,4 +113,10 @@ void LivingRoom::LivingRoom::update()
 {
     _livingRoomLight->update();
     _livingRoomGarland->update();
+    _temperature->update();
+    _humidity->update();
+    _airQuality->update();
+    _co2->update();
+    _lightLevel->update();
+    _humanDetector->update();
 }
--- src/living_room/living_room.h
+++ src/living_room/living_room.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <Arduino.h>
+#include <binary_sensor/binary_sensor.h>
 #include <device/wb_mr6c.h>
 #include <discovery.h>
 #include <mqtt.h>
 #include <light/relay_light.h>
 #include <relay/wb_mr6c.h>
+#include <sensor/sensor.h>
 #include <wirenboard.h>
 
 #include "./config.h"
@@ -31,5 +33,12 @@ namespace LivingRoom
 
         EDCommon::Light::Relay* _livingRoomLight = nullptr;
         EDCommon::Light::Relay* _livingRoomGarland = nullptr;
+
+        EDCommon::BinarySensor::BinarySensor* _humanDetector = nullptr;
+        EDCommon::Sensor::Sensor* _temperature = nullptr;
+        EDCommon::Sensor::Sensor* _humidity = nullptr;
+        EDCommon::Sensor::Sensor* _airQuality = nullptr;
+        EDCommon::Sensor::Sensor* _co2 = nullptr;
+        EDCommon::Sensor::Sensor* _lightLevel = nullptr;
     };
 }
